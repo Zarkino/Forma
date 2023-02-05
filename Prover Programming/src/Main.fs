3ec748a10919e323d1606f0a1fd392b8810f0564
Zarkino <fhv2325@gmail.com>
2023-02-05 16:01:22 +0100

    Create F# Wrapper for React Component: Monaco Editor

--- Prover Programming/src/Main.fs
+++ Prover Programming/src/Main.fs
@@ -3,60 +3,20 @@ module Main
 open Feliz
 open App
 open Browser.Dom
-open Fable.Core
 open Fable.Core.JsInterop
-open Fable.React
+open MonacoEditor
 
 importSideEffects "./styles/global.scss"
-
-[<StringEnum>]
-type Theme =
-    | Light
-    | Dark
-
-type Size =
-    | String of string
-    | Number of int
-
-type MonacoEditorProps = {
-    DefaultValue: string option
-    DefaultLanguage: string option
-    Value: string option
-    DefaultPath: string option
-    Language: string option
-    Path: string option
-    Theme: Theme option
-    Line: int option
-    Loading: ReactElement option
-    Options: obj option           // monaco.editor.IStandaloneEditorConstructionOptions
-    OverrideServices: obj option  // monaco.editor.IEditorOverrideServices
-    SaveViewState: bool option
-    KeepCurrentModel: bool option
-    Width: Size option
-    Height: Size option
-    ClassName: string option
-    WrapperProps: obj option
-    BeforeMount: (obj -> unit) option
-    OnMount: (obj -> unit) option
-    OnChange: (obj -> unit) option
-    OnValidate: (obj list -> unit) option
-}
-
-let Editor =
-    Interop.reactApi.createElement (import "default" "@monaco-editor/react", createObj [
-        "width" ==> "100%"
-        "height" ==> "90vh"
-        "defaultValue" ==> "let x = 10\nlet y = 20"
-        "lang" ==> "fsharp"
-        "theme" ==> "vs-dark"
-    ])
-
-//console.log("Imported value", Editor)
 
 let main =
     Html.div [
         Html.h1 "Editor"
-        Editor
+        MonacoEditor.create [
+            MonacoEditor.Value "let x = 10\nlet f x = x * x"
+            MonacoEditor.Language "fsharp"
+            MonacoEditor.Height "90vh"
+            MonacoEditor.Options (createObj [ "minimap" ==> createObj ["enabled" ==> "false"] ])
+        ]
     ]
 
 let root = ReactDOM.createRoot(document.getElementById "root")
