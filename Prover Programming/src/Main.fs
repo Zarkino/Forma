b8f93cb7ef1de7e7f0dc73b0ead7df45de306752
Zarkino <fhv2325@gmail.com>
2023-02-08 16:52:52 +0100

    Added Bulma Navigation bar

--- Prover Programming/src/Main.fs
+++ Prover Programming/src/Main.fs
@@ -17,16 +17,38 @@ let Main () =
     Html.div [
         prop.className theme
         prop.children [
-            Html.h1 [
-                prop.text "Prover Programming"
-                prop.className theme
-            ]
-            Bulma.button.button [
-                prop.text "Switch Theme"
-                prop.style [
-                    style.color theme
+            Bulma.navbar [
+                Bulma.color.isPrimary
+                prop.children [
+                    Bulma.navbarBrand.div [
+                        Bulma.navbarItem.a [
+                            Html.img [ prop.src "https://bulma.io/images/bulma-logo-white.png"; prop.height 28; prop.width 112; ]
+                        ]
+                    ]
+                    Bulma.navbarMenu [
+                        Bulma.navbarStart.div [
+                            Bulma.navbarItem.a [ prop.text "Home" ]
+                            Bulma.navbarItem.a [ prop.text "Documentation" ]
+                            Bulma.navbarItem.a [ prop.text "About" ]
+                        ]
+                        Bulma.navbarEnd.div [
+                            prop.style [ style.marginRight 10 ]
+                            prop.children [
+                                Switch.checkbox [
+                                    prop.id "theme_switch"
+                                    color.isDark
+                                    switch.isRounded
+                                    switch.isMedium
+                                    prop.onClick (fun _ -> setTheme(if theme.Equals("light") then "dark" else "light"))
+                                ]
+                                Html.label [
+                                    prop.htmlFor "theme_switch"
+                                    prop.text ""
+                                ]
+                            ]
+                        ]
+                    ]
                 ]
-                prop.onClick (fun _ -> setTheme(if theme.Equals("light") then "dark" else "light"))
             ]
             Bulma.columns [
                 Bulma.column [
