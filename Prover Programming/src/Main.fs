13203d33229acabef6a5d23a1b50befdfcaa295a
Zarkino <fhv2325@gmail.com>
2023-02-18 15:40:16 +0100

    Replace inner div with react fragment Added content to footer

--- Prover Programming/src/Main.fs
+++ Prover Programming/src/Main.fs
@@ -14,62 +14,63 @@ let Main () =
     let (theme, setTheme) = React.useState("light")
     let (value, setValue) = React.useState("let x = 10\nlet f x = x * x")
     
-    Html.div [
-        prop.className theme
-        prop.children [
-            Navigation_Bar.Navigation(theme, setTheme)
-            Button_Level.Button_Level(theme)
-            Bulma.columns [
-                columns.isGapless
-                prop.className theme
-                prop.children [
-                    Bulma.column [
-                        column.isHalf
-                        prop.children [
-                            Bulma.columns [
-                                columns.isMultiline
-                                prop.children [
-                                    Bulma.column [
-                                        column.isFull
-                                        prop.children [
-                                            Bulma.subtitle [
-                                                title.is4
-                                                prop.text "Input"
-                                            ]
+    React.fragment [
+        Navigation_Bar.Navigation(theme, setTheme)
+        Button_Level.Button_Level(theme)
+        Bulma.columns [
+            columns.isGapless
+            prop.className theme
+            prop.style [
+                style.marginBottom 0
+                style.paddingBottom (length.rem 1.5)
+            ]
+            prop.children [
+                Bulma.column [
+                    column.isHalf
+                    prop.children [
+                        Bulma.columns [
+                            columns.isMultiline
+                            prop.children [
+                                Bulma.column [
+                                    column.isFull
+                                    prop.children [
+                                        Bulma.subtitle [
+                                            title.is4
+                                            prop.text "Input"
                                         ]
                                     ]
-                                    Bulma.column [
-                                        column.isFull
-                                        prop.className "editor"
-                                        prop.children [
-                                            Components.Editor(theme, value, setValue)
-                                        ]
+                                ]
+                                Bulma.column [
+                                    column.isFull
+                                    prop.className "editor"
+                                    prop.children [
+                                        Components.Editor(theme, value, setValue)
                                     ]
                                 ]
                             ]
                         ]
                     ]
-                    Bulma.column [
-                        column.isHalf
-                        prop.children [
-                            Bulma.columns [
-                                columns.isMultiline
-                                prop.children [
-                                    Bulma.column [
-                                        column.isFull
-                                        prop.children [
-                                            Bulma.subtitle [
-                                                title.is4
-                                                prop.text "Output"
-                                            ]
+                ]
+                Bulma.column [
+                    column.isHalf
+                    prop.children [
+                        Bulma.columns [
+                            columns.isMultiline
+                            prop.children [
+                                Bulma.column [
+                                    column.isFull
+                                    prop.children [
+                                        Bulma.subtitle [
+                                            title.is4
+                                            prop.text "Output"
                                         ]
                                     ]
-                                    Bulma.column [
-                                        column.isFull
-                                        prop.className "editor"
-                                        prop.children [
-                                            Components.Editor(theme, marko_polo (value), readonly=true)
-                                        ]
+                                ]
+                                Bulma.column [
+                                    column.isFull
+                                    prop.className "editor"
+                                    prop.children [
+                                        Components.Editor(theme, marko_polo (value), readonly=true)
                                     ]
                                 ]
                             ]
@@ -77,13 +78,22 @@ let Main () =
                     ]
                 ]
             ]
-            Bulma.footer [
-                Bulma.color.hasBackgroundPrimary
-                prop.style [
-                    style.paddingTop 0
-                    style.paddingBottom 0
+        ]
+        Bulma.footer [
+            Bulma.color.hasBackgroundPrimary
+            prop.style [
+                style.textAlign.center
+                style.paddingTop 0
+                style.paddingBottom 0
+            ]
+            prop.children [
+                Html.p [
+                    Html.text "Developed by "
+                    Html.strong "s204433"
+                    Html.text " and "
+                    Html.strong "s204442"
+                    Html.text " at the Technical University of Denmark"
                 ]
-                prop.text "DTU copyright or something idk"
             ]
         ]
     ]
