e62e08c812795f703da038ea3505de4618bf8398
Zarkino <fhv2325@gmail.com>
2023-02-22 12:28:08 +0100

    Add button margin, remove subtitles & fix editor height

--- Prover Programming/src/Button Level.fs
+++ Prover Programming/src/Button Level.fs
@@ -8,27 +8,35 @@ let Button_Level(theme: string) =
     let decide_color theme = if theme.Equals("dark") then Bulma.color.isDark else Bulma.color.isLight
     
     Bulma.level [
-        prop.className theme
-        prop.style [ style.margin 0 ]
+        prop.className [
+            theme
+            "py-2"
+        ]
+        prop.style [style.margin 0]
         prop.children [
             Bulma.levelLeft [
                 Bulma.button.button [
+                    prop.className "mx-1"
                     decide_color theme
                     prop.text "Format"
                 ]
                 Bulma.button.button [
+                    prop.className "mx-1"
                     decide_color theme
                     prop.text "Download"
                 ]
                 Bulma.button.button [
+                    prop.className "mx-1"
                     decide_color theme
                     prop.text "Upload"
                 ]
                 Bulma.button.button [
+                    prop.className "mx-1"
                     decide_color theme
                     prop.text "Help"
                 ]
                 Bulma.button.button [
+                    prop.className "mx-1"
                     decide_color theme
                     prop.text "Examples"
                 ]
