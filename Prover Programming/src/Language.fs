d7378de080ab8469ea854bacf95e4d80a0b5b517
Zarkino <fhv2325@gmail.com>
2023-03-09 19:51:06 +0100

    Fix recursion

--- Prover Programming/src/Language.fs
+++ Prover Programming/src/Language.fs
@@ -12,37 +12,65 @@ module Grammar =
         | Equivalence of Formula * Formula
 
     let (formula: Parser<Formula, obj>), formulaRef = createParserForwardedToRef()
+
+    let stringLiteral: Parser<string,obj> =
+        let escape =  anyOf "\"\\/bfnrt"
+                      |>> function
+                          | 'b' -> "\b"
+                          | 'f' -> "\u000C"
+                          | 'n' -> "\n"
+                          | 'r' -> "\r"
+                          | 't' -> "\t"
+                          | c   -> string c // every other char is mapped to itself
+
+        let unicodeEscape =
+            /// converts a hex char ([0-9a-fA-F]) to its integer number (0-15)
+            let hex2int c = (int c &&& 15) + (int c >>> 6)*9
+
+            pstring "u" >>. pipe4 hex hex hex hex (fun h3 h2 h1 h0 ->
+                (hex2int h3)*4096 + (hex2int h2)*256 + (hex2int h1)*16 + hex2int h0
+                |> char |> string
+            )
+
+        let escapedCharSnippet = pstring "\\" >>. (escape <|> unicodeEscape)
+        let normalCharSnippet  = manySatisfy (fun c -> c <> '"' && c <> '\\')
+
+        between (pstring "\"") (pstring "\"")
+                (stringsSepBy normalCharSnippet escapedCharSnippet)
     
-    let variable: Parser<Formula, obj> =
-        (anyChar |> manyChars) |>> Variable
-        
-    let primary =
+    let variable = stringLiteral |>> Variable
+    let negation = (skipString "!") >>. formula |>> Negation
+    let conjunction = (formula .>> skipString "&") .>>. formula |>> Conjunction
+    let disjunction = (formula .>> skipString "|") .>>. formula |>> Disjunction
+    let implication = (formula .>> skipString "->") .>>. formula |>> Implication
+    let equivalence = (formula .>> skipString "<->") .>>. formula |>> Equivalence
+    
+    do formulaRef.Value <-
         choice [
             variable
+            negation
+            conjunction
+            disjunction
+            implication
+            equivalence
             between (skipChar '(') (skipChar ')') formula
         ]
 
-    let negation: Parser<Formula, obj> =
-        (skipChar '!' >>. anyChar |> manyChars) |>> fun formula -> Negation(Variable formula)
-    
-    let conjunction: Parser<Formula, obj> =
-        (anyChar |> manyCharsTill <| skipChar '&')
-        .>>.
-        (anyChar |> manyChars)
-        |>> fun (name, value) -> Conjunction(Variable(name), Variable(value))
-
-module PL =
+module Parser =
     open Parsec
     
     open Grammar
     
-    let result input = run variable input
+    // Discard leading whitespace and ensure the parser reaches end of stream
+    let expression = spaces >>. formula >>. spaces .>> eof
     
-    let tryParse source =
-        // Discard leading whitespace and ensure the parser reaches end of stream
-        let variable = spaces >>. variable .>> eof
-        
-        fun state ->
-            match run variable state source with
+    let parseExpression input = runString expression input
+    
+    let tryParse input =
+        match runString expression None input with
             | Ok(_, s, state)   -> Ok((), s, state)
             | Error e           -> Error e
+    
+    let x = match tryParse "!a" with
+            | Ok(v, _, _)   -> sprintf "%A" v
+            | Error(e)      -> sprintf "%A" e
