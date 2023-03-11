8c15e3ee068bac9c5096a90b8c794fab9cb49ca3
Zarkino <fhv2325@gmail.com>
2023-03-11 09:54:44 +0100

    Move AST to its own file

--- Prover Programming/src/Language.fs
+++ Prover Programming/src/Language.fs
@@ -3,13 +3,7 @@
 module Grammar =
     open Parsec
     
-    type Formula =
-        | Variable of string
-        | Negation of Formula
-        | Conjunction of Formula * Formula
-        | Disjunction of Formula * Formula
-        | Implication of Formula * Formula
-        | Equivalence of Formula * Formula
+    open Propositional_Logic
     
     let formula, formulaRef = createParserForwardedToRef()
     
---
+++ Prover Programming/src/Propositional Logic.fs
@@ -0,0 +1,9 @@
+﻿module Propositional_Logic
+
+type Formula =
+    | Variable of string
+    | Negation of Formula
+    | Conjunction of Formula * Formula
+    | Disjunction of Formula * Formula
+    | Implication of Formula * Formula
+    | Equivalence of Formula * Formula
