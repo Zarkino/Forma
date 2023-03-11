dad6ffeefe8360a83cd836dd604a5a95d2332303
Zarkino <fhv2325@gmail.com>
2023-03-11 09:32:24 +0100

    Remove unused code

--- Prover Programming/src/Language.fs
+++ Prover Programming/src/Language.fs
@@ -13,50 +13,8 @@ module Grammar =
     
     let formula, formulaRef = createParserForwardedToRef()
     
-    let stringLiteral: Parser<string,obj> =
-        let escape =  anyOf "\"\\/bfnrt"
-                      |>> function
-                          | 'b' -> "\b"
-                          | 'f' -> "\u000C"
-                          | 'n' -> "\n"
-                          | 'r' -> "\r"
-                          | 't' -> "\t"
-                          | c   -> string c // every other char is mapped to itself
-
-        let unicodeEscape =
-            /// converts a hex char ([0-9a-fA-F]) to its integer number (0-15)
-            let hex2int c = (int c &&& 15) + (int c >>> 6)*9
-
-            pstring "u" >>. pipe4 hex hex hex hex (fun h3 h2 h1 h0 ->
-                (hex2int h3)*4096 + (hex2int h2)*256 + (hex2int h1)*16 + hex2int h0
-                |> char |> string
-            )
-
-        let escapedCharSnippet = pstring "\\" >>. (escape <|> unicodeEscape)
-        let normalCharSnippet  = manySatisfy (fun c -> c <> '"' && c <> '\\')
-
-        between (pstring "\"") (pstring "\"")
-                (stringsSepBy normalCharSnippet escapedCharSnippet)
-    
     let variable = regex "\s*^[a-z]" |>> Variable
     let negation = pstring "!" >>. formula |>> Negation
-    let conjunction = (formula .>> pstring "&") .>>. formula |>> Conjunction
-    let disjunction = (formula .>> pstring "|") .>>. formula |>> Disjunction
-    let implication = (formula .>> pstring "->") .>>. formula |>> Implication
-    let equivalence = (formula .>> pstring "<->") .>>. formula |>> Equivalence
-    
-    (*
-    do formulaRef.Value <-
-        choice [
-            variable
-            negation
-            conjunction
-            disjunction
-            implication
-            equivalence
-            between (skipChar '(') (skipChar ')') formula
-        ]
-    *)
     
     let binaryFormula operator = spaces >>. pstring operator .>> spaces >>. formula
     
