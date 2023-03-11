2cb578643f3309db585577e046c8df28c7bb35c7
Zarkino <fhv2325@gmail.com>
2023-03-11 09:45:49 +0100

    Move spaces from regex in variable Simplify negation

--- Prover Programming/src/Language.fs
+++ Prover Programming/src/Language.fs
@@ -13,16 +13,15 @@ module Grammar =
     
     let formula, formulaRef = createParserForwardedToRef()
     
-    let variable = regex "\s*^[a-z]" |>> Variable
-    let negation = pstring "!" >>. formula |>> Negation
+    let variable = spaces >>. regex "^[a-z]" |>> Variable
+    let negation = spaces >>. pstring "!" >>. spaces >>. (variable <|> formula) |>> Negation
     
     let binaryFormula operator = spaces >>. pstring operator .>> spaces >>. formula
     
     do formulaRef.Value <- parse {
         let! left = choice [
             variable
-            pstring "!" >>. variable |>> Negation
-            pstring "!" >>. formula |>> Negation
+            negation
             pchar '(' >>. formula .>> pchar ')'
         ]
         return! choice [
@@ -39,11 +38,8 @@ module Parser =
     
     open Grammar
     
-    let tryParse input =
-        match runString formula () input with
-            | Ok(v, s, state)   -> Ok(v, s, state)
-            | Error e           -> Error e
+    let parse input = runString formula () input
     
-    let x = match tryParse "!(a -> !b)" with
+    let x = match parse "!(a -> !b)" with
             | Ok(v, s, _)   -> sprintf $"Success: %A{v}\nRemaining: %s{s.Value}"
             | Error(e)      -> sprintf $"Error: %A{e}"
