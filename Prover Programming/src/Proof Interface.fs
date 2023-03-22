b9410cd6c7d9dc70e454e108b816d7f2d5d2d0c6
Zarkino <fhv2325@gmail.com>
2023-03-22 17:02:15 +0100

    Add some rules

--- Prover Programming/src/Proof Interface.fs
+++ Prover Programming/src/Proof Interface.fs
@@ -15,3 +15,21 @@ type Lemma = {
     Identifier: Formula
     Proof: Proof
 }
+
+let mutable rules = [
+    ("Con_I",   [Variable("0"); Variable("1")],                                                         [Conjunction(Variable("0"), Variable("1"))])
+    ("Con_E1",  [Conjunction(Variable("0"), Variable("1"))],                                            [Variable("0")])
+    ("Con_E2",  [Conjunction(Variable("0"), Variable("1"))],                                            [Variable("1")])
+    ("Dis_I1",  [Variable("0")],                                                                        [Disjunction(Variable("0"), Variable("1"))])
+    ("Dis_I2",  [Variable("1")],                                                                        [Disjunction(Variable("0"), Variable("1"))])
+    ("Dis_E1",  [Disjunction(Variable("0"), Variable("1"))],                                            [Variable("0")])
+    ("Dis_E2",  [Disjunction(Variable("0"), Variable("1"))],                                            [Variable("1")])
+    //("Imp_I",   [Variable("0"); Variable("1")],                                                         [Implication(Variable("0"), Variable("1"))])
+    ("Imp_E",   [Implication(Variable("0"), Variable("1")); Variable("0")],                             [Variable("1")])
+    ("MT",      [Implication(Variable("0"), Variable("1")); Negation(Variable("1"))],                   [Negation(Variable("0"))])
+    ("Eqv_I",   [Implication(Variable("0"), Variable("1")); Implication(Variable("1"), Variable("0"))], [Equivalence(Variable("0"), Variable("1"))])
+    ("Eqv_E1",  [Equivalence(Variable("0"), Variable("1"))],                                            [Implication(Variable("0"), Variable("1"))])
+    ("Eqv_E2",  [Equivalence(Variable("0"), Variable("1"))],                                            [Implication(Variable("1"), Variable("0"))])
+    ("DNe_I",   [Variable("0")],                                                                        [Negation(Negation(Variable("0")))])
+    ("DNe_E",   [Negation(Negation(Variable("0")))],                                                    [Variable("0")])
+]
