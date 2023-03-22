bd79d2dbb67d45083fe3adbbe86d12dc34d3b39a
Zarkino <fhv2325@gmail.com>
2023-03-22 17:01:37 +0100

    Function to replace variable names with numbers

--- Prover Programming/src/Propositional Logic.fs
+++ Prover Programming/src/Propositional Logic.fs
@@ -7,3 +7,37 @@ type Formula =
     | Disjunction of Formula * Formula
     | Implication of Formula * Formula
     | Equivalence of Formula * Formula
+    static member Extract formula =
+        let rec f x =
+            match x with
+            | Variable(p)       -> seq { p }
+            | Negation(p)       -> f p
+            | Conjunction(p, q)
+            | Disjunction(p, q)
+            | Implication(p, q)
+            | Equivalence(p, q) -> seq { yield! f p; yield! f q }
+        f formula
+    static member Replace (formula, old_val, new_val) =
+        let rec f (x, o, n) =
+            match x with
+            | x when x = o  -> n
+            | Variable _        -> x
+            | Negation(p)       -> Negation(f(p, o, n))
+            | Conjunction(p, q) -> Conjunction(f(p, o, n), f(q, o, n))
+            | Disjunction(p, q) -> Disjunction(f(p, o, n), f(q, o, n))
+            | Implication(p, q) -> Implication(f(p, o, n), f(q, o, n))
+            | Equivalence(p, q) -> Equivalence(f(p, o, n), f(q, o, n))
+        f (formula, old_val, new_val)
+    static member ReplaceVar (formula, old_val, new_val) =
+        let rec f (x, o, n) =
+            match x with
+            | Variable(p) when p = o    -> Variable(n)
+            | Variable _                -> x
+            | Negation(p)               -> Negation(f(p, o, n))
+            | Conjunction(p, q)         -> Conjunction(f(p, o, n), f(q, o, n))
+            | Disjunction(p, q)         -> Disjunction(f(p, o, n), f(q, o, n))
+            | Implication(p, q)         -> Implication(f(p, o, n), f(q, o, n))
+            | Equivalence(p, q)         -> Equivalence(f(p, o, n), f(q, o, n))
+        f (formula, old_val, new_val)
+
+let f formula = (formula, Formula.Extract formula |> Seq.indexed) ||> Seq.fold (fun x (i, v) -> Formula.ReplaceVar (x, v, $"%i{i}"))
