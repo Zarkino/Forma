a182a201ecc42da0b120d704a9cfc02bfb85e24e
Zarkino <fhv2325@gmail.com>
2023-04-19 20:15:10 +0200

    Added tests for evaluation

---
+++ Prover Programming/tests/Evaluation.fs
@@ -0,0 +1,39 @@
+﻿module Tests.Evaluation
+
+open Fable.Mocha
+
+open Language.Parser
+open Proof_Interface
+
+let evaluating_Proofs =
+    testList "Basic Proof Evaluation Tests" [
+        let error_msg = "Evaluation failed"
+        test "Prove Proof: Empty" {
+            match parse_proof "proof {}" with
+            | None, _           -> Test.failtest "Could not be parsed"
+            | Some(proof), _    -> match prove(proof, Set.empty, Map.empty) with
+                                   | Success _  -> Expect.pass()
+                                   | _          -> Test.failtest error_msg
+        }
+        test "Prove Proof: Conjunction Swap" {
+            match parse_proof "proof {\n\tassume P & Q\n\thave P by Con_E1\n\thave Q by Con_E2\n\tshow Q & P by Con_I\n}" with
+            | None, _           -> Test.failtest "Could not be parsed"
+            | Some(proof), _    -> match prove(proof, Set.empty, rules) with
+                                   | Success _  -> Expect.pass()
+                                   | _          -> Test.failtest error_msg
+        }
+        test "Prove Proof: Double Negation" {
+            match parse_proof "proof {\n\tassume P\n\tproof {\n\t\tassume ~P\n\t\tshow ~P -> F by Abs_N\n\t}\n\tshow ~~P by Neg_I\n}" with
+            | None, _           -> Test.failtest "Could not be parsed"
+            | Some(proof), _    -> match prove(proof, Set.empty, rules) with
+                                   | Success _  -> Expect.pass()
+                                   | _          -> Test.failtest error_msg
+        }
+        test "Prove Proof: Bi-implication Swap" {
+            match parse_proof "proof {\n\tassume P <-> Q\n\tproof {\n\t\tassume Q\n\t\tshow Q -> P by Iff_E2\n\t}\n\tproof {\n\t\tassume P\n\t\tshow P -> Q by Iff_E1\n\t}\n\tshow Q <-> P by Iff_I\n}" with
+            | None, _           -> Test.failtest "Could not be parsed"
+            | Some(proof), _    -> match prove(proof, Set.empty, rules) with
+                                   | Success _  -> Expect.pass()
+                                   | _          -> Test.failtest error_msg
+        }
+    ]
--- Prover Programming/tests/Tests.fs
+++ Prover Programming/tests/Tests.fs
@@ -1,86 +1,14 @@
 ﻿module Tests
 
 open Fable.Mocha
-open Language.Parser
 
-open Propositional_Logic
+open Tests.Parsing
+open Tests.Evaluation
 
-let test_formula_parsing =
-    testList "Basic Formula Parsing Tests" [
-        let error_msg = "Incorrect parsing"
-        test "Parse Constant true" {
-            Expect.equal (parse_formula "T") (Some(Constant(true)), "") error_msg
-        }
-        test "Parse Constant false" {
-            Expect.equal (parse_formula "F") (Some(Constant(false)), "") error_msg
-        }
-        test "Parse Variables (Lowercase)" {
-            ['a'..'z'] |> List.map string |> List.iter (fun x -> Expect.equal (parse_formula x) (Some(Variable(x)), "") error_msg)
-        }
-        test "Parse Variables (Uppercase)" {
-            ['A'..'E']@['G'..'S']@['U'..'Z'] |> List.map string |> List.iter (fun x -> Expect.equal (parse_formula x) (Some(Variable(x)), "") error_msg)
-        }
-        test "Parse Negation (Single)" {
-            Expect.equal (parse_formula "~P") (Some(Negation(Variable("P"))), "") error_msg
-        }
-        test "Parse Negation (Multiple)" {
-            Expect.equal (parse_formula "~~~P") (Some(Negation(Negation(Negation(Variable("P"))))), "") error_msg
-        }
-        test "Parse Conjunction" {
-            Expect.equal (parse_formula "P & Q") (Some(Conjunction(Variable("P"), Variable("Q"))), "") error_msg
-        }
-        test "Parse Disjunction" {
-            Expect.equal (parse_formula "P | Q") (Some(Disjunction(Variable("P"), Variable("Q"))), "") error_msg
-        }
-        test "Parse Implication" {
-            Expect.equal (parse_formula "P -> Q") (Some(Implication(Variable("P"), Variable("Q"))), "") error_msg
-        }
-        test "Parse Bi-implication" {
-            Expect.equal (parse_formula "P <-> Q") (Some(Equivalence(Variable("P"), Variable("Q"))), "") error_msg
-        }
-    ]
+// Tests parsing
+Mocha.runTests parsing_PL |> ignore
+Mocha.runTests parsing_Proofs |> ignore
+Mocha.runTests parsing_ML |> ignore
 
-Mocha.runTests test_formula_parsing |> ignore
-
-open Proof_Interface
-
-let test_proof_parsing =
-    testList "Basic Proof Parsing Tests" [
-        let error_msg = "Incorrect parsing"
-        test "Parse Empty Proof" {
-            Expect.equal (parse_proof "proof {}") (Some([]), "") error_msg
-        }
-        test "Parse Rules" {
-            rules.Keys |> Seq.iter (fun rule -> Expect.equal
-                                                    (parse_proof (sprintf "proof {have P by %s}" rule))
-                                                    (Some([Intermediate(Variable("P"), rule)]), "") error_msg)
-        }
-        test "Parse Simple Proof" {
-            Expect.equal
-                (parse_proof "proof {assume P & Q\n\thave P by Con_E1\n\thave Q by Con_E2\n\tshow Q & P by Con_I}")
-                (Some([Assumption(Conjunction(Variable("P"), Variable("Q")))
-                       Intermediate(Variable("P"), "Con_E1")
-                       Intermediate(Variable("Q"), "Con_E2")
-                       Conclusion(Conjunction(Variable("Q"), Variable("P")), "Con_I")]), "") error_msg
-        }
-    ]
-
-Mocha.runTests test_proof_parsing |> ignore
-
-open Meta_Logic
-
-let test_meta_parsing =
-    testList "Basic Meta-logic Parsing Tests" [
-        let error_msg = "Incorrect parsing"
-        test "Parse Implication" {
-            Expect.equal (parse_meta "p ==> p") (Some(Implication(Formula(Variable("p")), Formula(Variable("p")))), "") error_msg
-        }
-        test "Parse Equality" {
-            Expect.equal (parse_meta "p == p") (Some(Equality(Formula(Variable("p")), Formula(Variable("p")))), "") error_msg
-        }
-        test "Parse Universal Quantifier" {
-            Expect.equal (parse_meta "!!x. p") (Some(Universal("x", Formula(Variable("p")))), "") error_msg
-        }
-    ]
-
-Mocha.runTests test_meta_parsing |> ignore
+// Tests evaluation
+Mocha.runTests evaluating_Proofs |> ignore
