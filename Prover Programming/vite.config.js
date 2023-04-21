b9550e6f92717de0e807b536bf9b3f68ae282a0b
Frederik Krogsdal Jacobsen <fkjac@dtu.dk>
2023-04-21 14:20:02 +0200

    Add option to build app as a single file.

--- Prover Programming/vite.config.js
+++ Prover Programming/vite.config.js
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
-export default defineConfig({
-    plugins: [react()],
-    root: "./src",
-    build: {
-        outDir: "../dist",
-    },
+export default defineConfig(({command,mode,ssrBuild}) => {
+    if (mode === 'single-file') {
+		return {
+	    	plugins: [react(), viteSingleFile()],
+	    	root: "./src",
+	    	build: {
+				outDir: "../dist-single-file",
+	    	}
+		}
+    } else {
+		return {
+		    plugins: [react()],
+		    root: "./src",
+		    build: {
+				outDir: "../dist",
+		    }
+		}
+    }
 })
