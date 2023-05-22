2e7892c69f1e2435929c11f30d730e004a71b5a4
Zarkino <fhv2325@gmail.com>
2023-05-22 16:21:51 +0200

    Fix evaluation tests

--- Prover Programming/tests/Evaluation.fs
+++ Prover Programming/tests/Evaluation.fs
@@ -10,7 +10,7 @@ let evaluating_Proofs =
     let evaluate goal proof =
         match runString Language.Proof.proof () proof with
             | Error _       ->  Test.failtest "Could not be parsed"
-            | Ok(v, _, _)   ->  match prove(goal, v, Set.empty, rules) with
+            | Ok(v, _, _)   ->  match prove(Logic.ML.Entity(goal), v, Set.empty, rules) with
                                 | Ok _  -> Expect.pass()
                                 | _     -> Test.failtest "Evaluation failed"
 
@@ -43,19 +43,37 @@ let evaluating_Proofs =
             let goal = Equivalence(Equivalence(Variable("P"), Variable("Q")), Equivalence(Variable("Q"), Variable("P")))
             let proof =
                 "proof (rule Iff_I) {
+				    assume P <-> Q
 					show (P <-> Q) -> (Q <-> P)
 					proof (rule Imp_I) {
 						assume P <-> Q
-						from P <-> Q have P -> Q by Iff_E1
-						from P <-> Q have Q -> P by Iff_E2
-						from P -> Q and Q -> P show Q <-> P by Iff_I
+				        show P -> Q
+				        proof (rule Imp_I) {
+				            assume P
+				            show Q by Iff_E1
+				        }
+				        show Q -> P
+				        proof (rule Imp_I) {
+				            assume Q
+				            show P by Iff_E2
+				        }
+						show Q <-> P by Iff_I
 					}
+				    assume Q <-> P
 					show (Q <-> P) -> (P <-> Q)
 					proof (rule Imp_I) {
 						assume Q <-> P
-						from Q <-> P have Q -> P by Iff_E1
-						from Q <-> P have P -> Q by Iff_E2
-						from Q -> P and P -> Q show P <-> Q by Iff_I
+				        show Q -> P
+				        proof (rule Imp_I) {
+				            assume Q
+				            show P by Iff_E1
+				        }
+						show P -> Q
+				        proof (rule Imp_I) {
+				            assume P
+				            show Q by Iff_E2
+				        }
+						show P <-> Q by Iff_I
 					}
 				}"
             evaluate goal proof
