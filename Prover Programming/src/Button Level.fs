90a253fffeed2303752e2a15b15a83b859337ded
Zarkino <fhv2325@gmail.com>
2023-06-02 17:16:15 +0200

    Make rules table in Help modal static

--- Prover Programming/src/Button Level.fs
+++ Prover Programming/src/Button Level.fs
@@ -83,17 +83,24 @@ let Button_Level (theme: string, setInput: string -> unit) =
                                                 Html.th [ Html.abbr "Definition" ]
                                             ]
                                         ]
-                                        Html.tbody (
-                                            Proof_Interface.rules
-                                            |> Map.toList
-                                            |> List.map
-                                                (fun (k, rule) ->
-                                                    Html.tr [
-                                                        Html.td $"%s{k}"
-                                                        Html.td $"%s{Logic.ML.Meta.ToString rule}"
-                                                    ]
-                                                )
-                                        )
+                                        Html.tbody [
+                                            Html.tr [ Html.td "Imp_I";      Html.td "(P ⟹ Q) ⟹ P ⟶ Q" ]
+                                            Html.tr [ Html.td "Imp_E";      Html.td "P ⟶ Q ⟹ P ⟹ Q" ]
+                                            Html.tr [ Html.td "Con_I";      Html.td "P ⟹ Q ⟹ P ∧ Q" ]
+                                            Html.tr [ Html.td "Con_E1";     Html.td "P ∧ Q ⟹ P" ]
+                                            Html.tr [ Html.td "Con_E2";     Html.td "P ∧ Q ⟹ Q" ]
+                                            Html.tr [ Html.td "Dis_I1";     Html.td "P ⟹ P ∨ Q" ]
+                                            Html.tr [ Html.td "Dis_I2";     Html.td "Q ⟹ P ∨ Q" ]
+                                            Html.tr [ Html.td "Dis_E";      Html.td "P ∨ Q ⟹ (P ⟹ R) ⟹ (Q ⟹ R) ⟹ R" ]
+                                            Html.tr [ Html.td "Neg_I";      Html.td "(P ⟹ ⊥) ⟹ ¬P" ]
+                                            Html.tr [ Html.td "Neg_E";      Html.td "¬P ⟹ P ⟹ Q" ]
+                                            Html.tr [ Html.td "Iff_I";      Html.td "(P ⟹ Q) ⟹ (Q ⟹ P) ⟹ P ⟷ Q" ]
+                                            Html.tr [ Html.td "Iff_E1";     Html.td "P ⟷ Q ⟹ P ⟹ Q" ]
+                                            Html.tr [ Html.td "Iff_E2";     Html.td "P ⟷ Q ⟹ Q ⟹ P" ]
+                                            Html.tr [ Html.td "Falsity_E";  Html.td "⊥ ⟹ P" ]
+                                            Html.tr [ Html.td "Truth_I";    Html.td "⊥ ⟶ ⊥ ⟹ ⊤" ]
+                                            Html.tr [ Html.td "LEM";        Html.td "P ∨ ¬P" ]
+                                        ]
                                     ]
                                 ]
                             ]
