065c6fb0e622d5da483493bd3126b22847e494e2
Zarkino <fhv2325@gmail.com>
2023-06-19 17:51:42 +0200

    Simplify Footer

--- Prover Programming/src/Footer.fs
+++ Prover Programming/src/Footer.fs
@@ -8,14 +8,9 @@ let Footer() =
     let accent = React.useContext(Contexts.accentContext)
     
     Bulma.footer [
-            Bulma.color.hasTextLight
-            prop.style [
-                style.textAlign.center
-                style.paddingTop 0
-                style.paddingBottom 0
-                style.backgroundColor accent
-            ]
-            prop.children [
-                Html.div [ prop.style [ style.height 20 ] ]
-            ]
+        prop.style [
+            style.backgroundColor accent
+            style.padding 0
+            style.height 20
         ]
+    ]
