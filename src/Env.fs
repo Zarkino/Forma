a8fd26080a4e1602eac1ebadeaafaa19f27829de
Zarkino <fhv2325@gmail.com>
2026-09-04 19:32:03 +0200

    Add Vite environment variables

---
+++ src/Env.fs
@@ -0,0 +1,20 @@
+﻿namespace Env
+
+open Fable.Core
+
+[<RequireQualifiedAccess>]
+module Vite =
+    [<Emit("import.meta.env.MODE")>]
+    let MODE: string = jsNative
+
+    [<Emit("import.meta.env.BASE_URL")>]
+    let BASE_URL: string = jsNative
+    
+    [<Emit("import.meta.env.PROD")>]
+    let PROD: bool = jsNative
+
+    [<Emit("import.meta.env.DEV")>]
+    let DEV: bool = jsNative
+
+    [<Emit("import.meta.env.SSR")>]
+    let SSR: bool = jsNative
